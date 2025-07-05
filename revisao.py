9ea204b023633d3746e57d2d0efae1531bb22931
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-07-04 21:55:53 -0300

    :sparkles: feat: exercicios de lista

--- revisao.py
+++ revisao.py
@@ -1,1 +1,56 @@
 # Revisão de Array/listas
+
+# QUestões de listas/arrays
+
+#<--------------- QUESTÃO 1 --------------->
+
+"""
+Exercício 1: Soma dos Pares (Fácil)
+
+Crie um programa Python que receba uma lista de números inteiros. O programa deve calcular 
+e imprimir a soma de todos os números pares presentes na lista.
+
+Exemplo:
+
+Se a lista for [1, 2, 3, 4, 5, 6], a saída deve ser 12 (2 + 4 + 6).
+"""
+
+# inteiros = [int(input('Informe um número inteiro: ')) for i in range(6)]
+
+# resultado_pares = 0
+
+# for numero in inteiros:
+#     if numero % 2 == 1:
+#         resultado_pares += numero
+
+# print(resultado_pares)
+
+# -------------------------------------------
+
+#<--------------- QUESTÃO 2 --------------->
+
+"""
+Exercício 2: Inverter Lista Sem Usar Funções Built-in (Médio)
+
+Desenvolva um programa Python que receba uma lista de elementos 
+(pode ser de qualquer tipo). O programa deve criar e 
+retornar uma nova lista contendo os elementos da lista original na ordem inversa. 
+Você não pode usar funções built-in como reverse(), reversed() ou fatiamento [::-1].
+
+Exemplo:
+
+Se a lista for ['a', 'b', 'c', 'd'], a saída deve ser ['d', 'c', 'b', 'a'].
+"""
+
+# lista_inteiros = [1,2,3,4,5,6]
+
+# lista_reversa = lista_inteiros[-1::-1]
+
+# print(lista_reversa)
+
+
+
+lista = [1,2,3]
+
+
+
